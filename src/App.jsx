04b6ecd9e9932a6e89c8949e2987f3d16992138e
Brett Stetson <bstetson@visione.com>
2024-11-04 05:47:00 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        Logo
+        Alexander Landscaping & Design
+      </header>
+
+      <main>
+        Alexander Landscaping & Design
+        Serving Augusta and the CSRA since 2022!
+        Lawncare and maintenance, bush trimming, mulch and pine straw installation, overgrowth cleanups, pressure washing, you name it, Alexander Landscaping and Design has you covered!
+
+        Offering FREE QUOTES within the CSRA!
+
+        Free Quotes within the CSRA!
+        Lawn cutting | Leaf / seasonal cleanup | Landscape design | Mulching
+
+
+        View Gallery
+        Book Now
+        Book Now
+      </main>
+
+      <footer>
+        Alexander Landscaping & Design
+
+        706-288-8635
+
+        alexanderlandscaping122@gmail.com
+
+        Facebook
+        Google
+      </footer>
+    </>
+  )
+}
+
+export default App
